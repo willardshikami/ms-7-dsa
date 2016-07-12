1523f1c7f96545055108130bb33e2cd76b1bb050
Nelson Kimaiga <nelsonkimaiga@aol.com>
2016-07-12 09:45:53 +0300

    Update ThreeSort.java

--- ThreeSort.java
+++ ThreeSort.java
@@ -1,1 +1,10 @@
+// Create a ThreeSort class with a threeSort method and a main method.
+// The threeSort method should contain the algorithm and the main method
+// should be used for testing your algorithm. To make that easier, make the
+// threeSort method static.
 
+// Given three numbers as input, find the min, middle and max of the three.
+// Return an array of the numbers in ascending order.
+// Hint: to easily see your output, import java.util.Arrays
+// and use the Arrays.toString(array) method to convert the Array to a string
+// and then log it using System.out.println();
